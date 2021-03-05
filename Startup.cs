ecde05ec9ee3eb7a9ccd552e6e376d73727ab61d
mindaugaspo <mindaugas.poskus@gmail.com>
2021-03-05 07:03:23 +0200

    calculator ui and dummy input/output

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,8 @@ namespace LoanCalculator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
             services.AddControllersWithViews();
         }
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function calculate(amount) {
+    if (!amount) {
+        $("#output-container-content").html("Please enter a number");
+        return;
+    }
+    amount = amount.replace(/\s/g, '');
+    amount = amount.replace(",", ".");
+    if (isNaN(amount)) {
+        $("#output-container-content").html("Only numbers are accepted");
+        return;
+    }
+    $("#output-container-content").html(amount);
+    $("#history-container").html($("#history-container").html() + "\n" + amount);
+}
+$("#execute").on("click", function () {
+    calculate($("#command").val());
+});
+$("#command").on("keypress", function (e) {
+    if (e.which === 13) {
+        calculate($("#command").val());
+    }
+});
